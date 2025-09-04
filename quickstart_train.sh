051a02fa753678e5a9a851879a4773919b6eba7e
Bertil Braun <bertil.braun.private@gmail.com>
2025-09-04 12:53:02 +0100

    Create quickstart_train.sh

---
+++ quickstart_train.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/BertilBraun/Windsurf-Analysis.git
+cd Windsurf-Analysis
+git checkout production
+git pull
+pip install -r requirements.txt
+cd train/detection
+python train.py --src windsurf_dataset
