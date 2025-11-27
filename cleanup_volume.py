c59923889569b3c25cad80b4f418558f096662d2
Bertil Braun <bertil.braun.private@gmail.com>
2025-11-27 11:12:38 +0000

    Fix volume cleanup script from crashing with folders

--- cleanup_volume.py
+++ cleanup_volume.py
@@ -3,4 +3,7 @@ import modal
 # delete all files in the volume
 volume = modal.Volume.from_name('windsurf-analysis-volume', create_if_missing=True)
 for file in volume.listdir('/'):
+    extension = file.path.split('.')[-1]
+    if extension not in ['mp4', 'json', 'bin']:
+        continue
     volume.remove_file(file.path)
