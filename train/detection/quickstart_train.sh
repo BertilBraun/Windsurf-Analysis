1f7c73c776283d72c1dbc2fb361bbe072194db11
Bertil Braun <bertil.braun.private@gmail.com>
2026-02-04 23:11:10 +0000

    Updated quickstart

--- train/detection/quickstart_train.sh
+++ train/detection/quickstart_train.sh
@@ -1,9 +1,7 @@
 git clone https://github.com/BertilBraun/Windsurf-Analysis.git
 cd Windsurf-Analysis
-git checkout production
-git pull
 pip install -r requirements.txt
-python train/detection/train_pose.py --src train/detection/windsurf_dataset --pose train/detection/pose_projects/boom_mast_v1 --base-model yolo11m-pose.pt --device 0 --name pose_seed_500_epochs --epochs 500 --patience 0 --save-period 50
+python train/detection/train_pose.py --src train/detection/windsurf_dataset --pose train/detection/pose_projects/boom_mast_v1 --base-model yolo11m-pose.pt --device 0 --name pose_seed_800_epochs --epochs 800 --patience 0 --save-period 100
 
 
 
