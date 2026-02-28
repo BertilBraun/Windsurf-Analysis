baeefbda3903915d5e0f86934eb710c10e7e30ff
Bertil Braun <bertil.braun.private@gmail.com>
2026-02-28 10:33:37 +0000

    New large training run with new samples and mp4 artifacts

--- train/detection/quickstart_train.sh
+++ train/detection/quickstart_train.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/BertilBraun/Windsurf-Analysis.git
 cd Windsurf-Analysis
 pip install -r requirements.txt
-python train/detection/train_pose.py --src train/detection/windsurf_dataset --pose train/detection/pose_projects/boom_mast_v1 --base-model yolo11m-pose.pt --device 0 --name pose_seed_800_epochs --epochs 800 --patience 0 --save-period 100
+python train/detection/train_pose.py --src train/detection/windsurf_dataset --pose train/detection/pose_projects/boom_mast_v1 --base-model yolo11m-pose.pt --device 0 --name pose_seed_1000_epochs --epochs 1000 --patience 0 --save-period 200
 
 
 
